c57cf7e43b9a6ebe39f8786643155278070ea5c8
Priscilla H <priscillamhuynh@gmail.com>
2018-07-05 12:14:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
   attr_accessor :name, :artist
 
- def initialize(name)
-   @name = name
+  def initialize(name)
+    @name = name
   end
 
   def self.new_by_filename(file)
@@ -14,7 +14,7 @@ class Song
   end
 
   def artist_name=(name)
-    artist=Artist.find_or_create_by_name(name)
+    artist = Artist.find_or_create_by_name(name)
     self.artist = artist
     artist.add_song(self)
   end
